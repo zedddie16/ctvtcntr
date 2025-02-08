0abadfc6f0bfe53cab065a87b8e996c7b015cec7
tuturuu <zedddiezxc@gmail.com>
2025-02-08 20:23:51 +0100

    io fn implemented

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,37 @@ use std::io::BufReader;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
+use csv::WriterBuilder;
+use std::io::Write;
+
+fn write_usage_data(file_path: &str, usage_map: &HashMap<String, Duration>) -> io::Result<()> {
+    let file = File::create(file_path)?;
+    let mut csv_writer = WriterBuilder::new().has_headers(true).from_writer(file);
+
+    for (app_name, total_time) in usage_map {
+        let record = Usage {
+            window_name: app_name.clone(),
+            total_time: *total_time,
+        };
+        csv_writer.serialize(record)?;
+    }
+
+    csv_writer.flush()?;
+    Ok(())
+}
+
 #[derive(Deserialize, Serialize, Debug)]
 struct Usage {
     window_name: String,
     total_time: Duration,
 }
 
+fn update_usage(usage_map: &mut HashMap<String, Duration>, app_name: &str, time_spent: Duration) {
+    usage_map
+        .entry(app_name.to_string())
+        .and_modify(|e| *e += time_spent)
+        .or_insert(time_spent);
+}
 fn read_usage_data(file_path: &str) -> io::Result<HashMap<String, Duration>> {
     let mut usage_map = HashMap::new();
     let file = File::open(file_path)?;
@@ -28,7 +53,7 @@ fn read_usage_data(file_path: &str) -> io::Result<HashMap<String, Duration>> {
 
     Ok(usage_map)
 }
-fn main() {
+fn monitor_active_window() {
     let mut window_times: HashMap<String, Duration> = HashMap::new();
     let mut last_window_title: Option<String> = None;
     let mut last_switch_time = Instant::now();
@@ -50,9 +75,11 @@ fn main() {
 
                 println!("Switched to: {} at {:?}", window_title, last_switch_time);
             }
+            sleep(Duration::from_millis(500));
         }
-
-        sleep(Duration::from_millis(500));
     }
+}
+fn main() {
+    monitor_active_window()
 }
 
