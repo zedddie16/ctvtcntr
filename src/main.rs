7ededacc0994acbf84d187e3e7b35fdf02e71ee4
zedddie16 <zedddiezxc@gmail.com>
2025-05-24 19:48:55 +0200

    feat: build dynamic app path from build script

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod startup;
 use startup::wait_for_hyprland_socket;
 mod match_title;
 
+use std::env;
+
 fn main() -> io::Result<()> {
     tracing::subscriber::set_global_default(tracing_subscriber::FmtSubscriber::new())
         .expect("setting default subscriber failed");
@@ -17,8 +19,13 @@ fn main() -> io::Result<()> {
         error!("Error waiting for Hyprland: {}", e);
         return Err(io::Error::new(io::ErrorKind::Other, e));
     }
+
+    // Load data path exposed by build script
+    let data_dir_str = env!("CTVTCNTR_DATA_DIR");
+    let app_usage_dir = format!("{}/app_usage.csv", data_dir_str);
+
     // Load existing usage data (if any), then start monitoring.
-    let mut usage_map = read_usage_data("app_usage.csv")?;
+    let mut usage_map = read_usage_data(&app_usage_dir)?;
     info!("readed usage data");
     monitor_active_window(&mut usage_map)
 }
