f270dce4047ff18d47a11a5c79231794ccc39b5e
zedddie16 <zedddiezxc@gmail.com>
2025-05-25 19:54:10 +0200

    refactor: move processing title logic to match_title module

--- src/match_title.rs
+++ src/match_title.rs
@@ -1,9 +1,46 @@
-// TODO: Remove manual check, improve Regex. (may be move these checks somwhere else as a big match statement.)
-
+use hyprland::data::Client;
+use regex::Regex;
 /// Normalizes the window title into a process name:
 /// - Trims whitespace.
 /// - If the title starts with "New Tab -", that prefix is removed (case‑insensitive).
 /// - If a " | " separator is present, only the part before it is used.
+pub fn process_complex_names(process_name: String, window: &Client) -> String {
+    let rexex_str = Regex::new(r"^(.+?)\s*–\s*").unwrap(); // trims active title of app
+    if window.class == "com.mitchellh.ghostty" {
+        if window.title.contains("nvim") {
+            return "NeoVim".to_string();
+        } else {
+            return "Ghostty".to_string();
+        }
+    }
+    // Handle Kitty windows running nvim
+    // TODO: move these checks to match_title module
+    if window.class == "kitty" {
+        if window.title.contains("nvim") {
+            return "Vim".to_string();
+        } else {
+            return "Kitty".to_string();
+        }
+    }
+
+    // TODO: move these checks to match_title module
+    if process_name.is_empty() {
+        if !window.class.is_empty() {
+            if process_name == "jetbrains-rustrover" {
+                if let Some(captures) = rexex_str.captures(window.title.as_str()) {
+                    if let Some(extracted_match) = captures.get(1) {
+                        return format!("RustRover -> {}", extracted_match.as_str());
+                    } else {
+                        return "RustRover".to_string();
+                    }
+                } else {
+                    return "RustRover".to_string();
+                }
+            }
+        }
+    }
+    process_name
+}
 pub fn extract_process_name(window_title: &str) -> String {
     let trimmed = &window_title.trim();
 
