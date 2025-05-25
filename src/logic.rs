00be687c48d40f2e969536b9449d50b9bd378df1
zedddie16 <zedddiezxc@gmail.com>
2025-05-25 19:53:03 +0200

    refactor: move processing title logic to match_title module

--- src/logic.rs
+++ src/logic.rs
@@ -1,10 +1,10 @@
 use crate::db::{self, log_activity};
 use crate::match_title::extract_process_name;
+use crate::match_title::process_complex_names;
 use chrono::Local;
 use csv::{ReaderBuilder, WriterBuilder};
 use hyprland::data::Client;
 use hyprland::shared::HyprDataActiveOptional;
-use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::fs::File;
@@ -24,24 +24,10 @@ pub struct Usage {
 }
 
 /// Updates the usage map by adding elapsed time for the given (date, window_name) key.
-fn update_usage(
-    usage_map: &mut BTreeMap<(String, String), Duration>,
-    date: &str,
-    window_name: &str,
-    elapsed: Duration,
-) {
-    let key = (date.to_string(), window_name.to_string());
-    usage_map
-        .entry(key)
-        .and_modify(|d| *d += elapsed)
-        .or_insert(elapsed);
-}
-
 /// Monitors the active window and updates usage data:
 /// - Uses chrono to get the current date.
 /// - If a record for the current date and process already exists, increments its usage time.
 /// - Otherwise, creates a new record for today.
-/// TODO: rewrite monitor active window logic to use duckdb
 pub fn monitor_active_window(conn: duckdb::Connection) -> io::Result<()> {
     let running = Arc::new(AtomicBool::new(true)); // creating app's state
     let r = running.clone();
@@ -54,8 +40,6 @@ pub fn monitor_active_window(conn: duckdb::Connection) -> io::Result<()> {
     let mut last_key: Option<(String, String)> = None; // (date, process name)
     let mut last_switch_time = Instant::now();
 
-    let rexex_str = Regex::new(r"^(.+?)\s*–\s*").unwrap(); // trims active title of app
-
     info!("starting active window monitor loop");
     while running.load(Ordering::SeqCst) {
         // loop start
@@ -63,62 +47,26 @@ pub fn monitor_active_window(conn: duckdb::Connection) -> io::Result<()> {
         if let Ok(Some(active_window)) = Client::get_active() {
             let raw_title = active_window.initial_title.clone();
             let mut process_name = extract_process_name(&raw_title);
-            // TODO: move these checks to match_title module
-            if active_window.class == "com.mitchellh.ghostty" {
-                if active_window.title.contains("nvim") {
-                    process_name = "NeoVim".to_string();
-                } else {
-                    process_name = "Ghostty".to_string();
-                }
-            }
-            // Handle Kitty windows running nvim
-            // TODO: move these checks to match_title module
-            if active_window.class == "kitty" {
-                if active_window.title.contains("nvim") {
-                    process_name = "Vim".to_string();
-                } else {
-                    process_name = "Kitty".to_string();
-                }
-            }
-
-            // TODO: move these checks to match_title module
-            if process_name.is_empty() {
-                if !active_window.class.is_empty() {
-                    process_name = active_window.class;
-                    if process_name == *"jetbrains-rustrover" {
-                        if let Some(captures) = rexex_str.captures(active_window.title.as_str()) {
-                            let extracted = captures.get(1).unwrap().as_str();
-                            process_name = format!("RustRover -> {}", extracted);
-                        } else {
-                            process_name = "RustRover".to_string();
-                        }
-                    }
-                } else {
-                    sleep(Duration::from_millis(500));
-                    continue;
-                }
-            }
+            process_name = process_complex_names(process_name, &active_window);
             let current_key = (current_date.clone(), process_name);
             if last_key.as_ref() != Some(&current_key) {
                 if let Some(ref prev_key) = last_key {
                     let elapsed = last_switch_time.elapsed();
-                    log_activity(&conn, &prev_key.1, elapsed.as_secs());
+                    log_activity(&conn, &prev_key.1, elapsed.as_secs())
+                        .expect("failed to log activity");
                 }
                 last_key = Some(current_key.clone());
                 last_switch_time = Instant::now();
                 info!("switched to: {}", current_key.1);
             }
         }
-
-        // Write updated usage data to CSV.
-        // write_usage_data("app_usage.csv", usage_map)?;
         sleep(Duration::from_millis(500));
     }
     // save last record before shutting down
     if let Some(ref final_key) = last_key {
         let elapsed_final = last_switch_time.elapsed();
         if let Err(e) = log_activity(&conn, &final_key.1, elapsed_final.as_secs()) {
-            error!("Failed to upsert final usage data on shutdown")
+            error!("Failed to upsert final usage data on shutdown: {e}")
         }
     } else {
         info!("Final usage data successfully written.");
