de05d8f5c2a6e1d7625f10f25b46c0026dc06be8
zedddie16 <zedddiezxc@gmail.com>
2025-05-27 09:49:05 +0200

    feat: slightly improved log messages

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use duckdb::Connection;
 use tracing::{error, info};
 
+mod errors;
+
 mod db;
 use db::{ensure_table_exists, print_all_records};
 
@@ -32,11 +34,15 @@ fn main() {
             panic!("Critical error: Could not set up application data directory.");
         }
     };
-    let conn = Connection::open(&path_to_database_file)
-        .expect("Failed to connect to a database on: {&path_to_database}");
+
+    info!("Trying to connect to DB at: {path_to_database_file:?}");
+    let conn = Connection::open(&path_to_database_file).expect("Failed to connect to DuckDB");
     info!("connected to duckdb on {path_to_database_file:?}");
+
+    info!("Trying to ensure Table 'activity_log'");
     ensure_table_exists(&conn).expect("ensuring failed");
     info!("Table 'activity_log' ensured.");
+
     print_all_records(&conn).expect("Failed to print_all_records");
     monitor_active_window(conn).expect("failed to start active monitor window loop");
 }
