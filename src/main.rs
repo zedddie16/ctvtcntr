1b302bda62416cbdbdff6c810dc1ab10ed8b9256
zedddie16 <zedddiezxc@gmail.com>
2025-07-25 10:41:41 +0200

    feat: add match_class

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use duckdb::Connection;
 use tracing::{error, info};
 
 mod errors;
+mod match_class;
 mod match_title;
+
 mod db;
 use db::{ensure_table_exists, print_all_records};
 
@@ -23,7 +25,7 @@ fn main() {
     if let Err(e) = wait_for_hyprland_socket(60) {
         error!("Error waiting for Hyprland: {}", e);
     }
-  
+
     let path_to_database_file = match get_app_data_dir() {
         Ok(mut app_data_dir) => {
             app_data_dir.push("records.db");
