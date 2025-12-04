129982ac60919ac599154ed1b428a8ff868faafa
zedddie16 <zedddiezxc@gmail.com>
2025-12-04 07:48:30 +0100

    feat: partially remove old match_title logic and construct WindowData struct for now

--- src/match_title.rs
+++ src/match_title.rs
@@ -10,7 +10,7 @@ pub struct WindowData {
 /// - Trims whitespace.
 /// - If the title starts with "New Tab -", that prefix is removed (case‑insensitive).
 /// - If a " | " separator is present, only the part before it is used.
-pub fn process_complex_names(process_name: String, window: &Client) -> String {
+pub fn process_complex_names(process_name: String, window: &Client) -> WindowData {
     //TODO: refactor fn to return both window_name and window_sub_name
     //
     // window_name comes from :
@@ -24,32 +24,43 @@ pub fn process_complex_names(process_name: String, window: &Client) -> String {
     // shortest prefix before " – " e.g. project name before separator.
     let rexex_str = Regex::new(r"^(.+?)\s*–\s*").unwrap(); // lol i just noticed i misspeled
                                                            // regex but it look funnny so I will keep it like that
-    if window.class == "com.mitchellh.ghostty" {
-        if window.title.contains("nvim") {
-            return "NeoVim".to_string();
-        } else {
-            return "Ghostty".to_string();
-        }
-    }
-    // Handle Kitty windows running nvim
-    if window.class == "kitty" {
-        if window.title.contains("nvim") {
-            return "Vim".to_string();
-        } else {
-            return "Kitty".to_string();
-        }
+
+    // For now Ill not process windowData but this will be major task later,
+    // as for now I wll just construct WindowData and return it to the logic module
+    let window_sub_name = window.title.clone();
+    WindowData {
+        window_name: process_name,
+        window_sub_name,
     }
 
-    if window.class == "jetbrains-rustrover" && process_name.is_empty() {
-        if let Some(cap) = rexex_str.captures(&window.title) {
-            if let Some(extracted_match) = cap.get(1) {
-                return format!("RustRover -> {}", extracted_match.as_str());
-            }
-        }
-        return "RustRover".to_string();
-    }
-
-    process_name
+    //
+    //
+    // if window.class == "com.mitchellh.ghostty" {
+    //     if window.title.contains("nvim") {
+    //         return "NeoVim".to_string();
+    //     } else {
+    //         return "Ghostty".to_string();
+    //     }
+    // }
+    // // Handle Kitty windows running nvim
+    // if window.class == "kitty" {
+    //     if window.title.contains("nvim") {
+    //         return "Vim".to_string();
+    //     } else {
+    //         return "Kitty".to_string();
+    //     }
+    // }
+    //
+    // if window.class == "jetbrains-rustrover" && process_name.is_empty() {
+    //     if let Some(cap) = rexex_str.captures(&window.title) {
+    //         if let Some(extracted_match) = cap.get(1) {
+    //             return format!("RustRover -> {}", extracted_match.as_str());
+    //         }
+    //     }
+    //     return "RustRover".to_string();
+    // }
+    //
+    // process_name
 }
 // this
 pub fn extract_process_name(window_title: &str) -> String {
